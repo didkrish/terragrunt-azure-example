b3c9e85241b0f0068f2337c6cde0f022ba7f9be3
subhransu <100188157+subhransu-1@users.noreply.github.com>
2022-09-12 11:03:44 +0530

    Update backend.tf

--- vs-enterprise/staging/global/resource_groups/backend.tf
+++ vs-enterprise/staging/global/resource_groups/backend.tf
@@ -4,7 +4,7 @@ terraform {
     container_name       = "environment-states"
     key                  = "vs-enterprise/staging/global/resource_groups/terraform.tfstate"
     resource_group_name  = "rg-terragrunt-example-001"
-    storage_account_name = "stterragruntexample001"
-    subscription_id      = "938cf79f-5038-404c-acad-239711618327"
+    storage_account_name = "stterragruntexample003"
+    subscription_id      = "0acb0567-53f9-4062-8e2e-4757e4814cd8"
   }
 }
